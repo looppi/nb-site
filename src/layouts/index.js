4cdb304f91272e789be29ce4e62eaf599f074311
Matti Pohjanvirta <matti.pohjanvirta@iki.fi>
2018-03-27 15:04:26 +0300

    add meta properties

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,20 @@ import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Northbound" />
+    <Helmet title="Northbound | Anna myyjiesi keskittyä olennaiseen">
+      <meta property="og:title" content="Northbound | Anna myyjiesi keskittyä olennaiseen"/>
+      <meta property="og:url" content="https://northbound.fi"/>
+      <meta property="og:description" content="Northbound ulkoistaa koko myyntiprosessin alkupään avaimet käteen -palveluksi. “Paras tuki, mitä B2B-myyjä on koskaan saanut!”"/>
+      <meta property="og:locale" content="fi_FI"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:image" content="https://northbound.fi/img/meta-image.png"/>
+      <meta property="twitter:card" content="summary"/>
+      <meta property="twitter:title" content="Northbound | Anna myyjiesi keskittyä olennaiseen"/>
+      <meta property="twitter:url" content="https://northbound.fi"/>
+      <meta property="twitter:image" content="https://northbound.fi/img/meta-image.png"/>
+      <meta property="twitter:description" content="Northbound ulkoistaa koko myyntiprosessin alkupään avaimet käteen -palveluksi. “Paras tuki, mitä B2B-myyjä on koskaan saanut!”"/>
+
+    </Helmet>
     <div>{children()}</div>
   </div>
 );
