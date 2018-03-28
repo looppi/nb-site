b1fa06af3a0a5ac0b95a35b68b079a640f87406d
Matti Pohjanvirta <matti.pohjanvirta@iki.fi>
2018-03-28 12:53:35 +0300

    update meta properties

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,15 +7,15 @@ import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Northbound | Anna myyjiesi keskittyä olennaiseen">
-      <meta property="og:title" content="Northbound | Anna myyjiesi keskittyä olennaiseen"/>
+    <Helmet title="Northbound">
+      <meta property="og:title" content="Northbound"/>
       <meta property="og:url" content="https://northbound.fi"/>
       <meta property="og:description" content="Northbound ulkoistaa koko myyntiprosessin alkupään avaimet käteen -palveluksi. “Paras tuki, mitä B2B-myyjä on koskaan saanut!”"/>
       <meta property="og:locale" content="fi_FI"/>
       <meta property="og:type" content="website"/>
       <meta property="og:image" content="https://northbound.fi/img/meta-image.png"/>
       <meta property="twitter:card" content="summary"/>
-      <meta property="twitter:title" content="Northbound | Anna myyjiesi keskittyä olennaiseen"/>
+      <meta property="twitter:title" content="Northbound"/>
       <meta property="twitter:url" content="https://northbound.fi"/>
       <meta property="twitter:image" content="https://northbound.fi/img/meta-image.png"/>
       <meta property="twitter:description" content="Northbound ulkoistaa koko myyntiprosessin alkupään avaimet käteen -palveluksi. “Paras tuki, mitä B2B-myyjä on koskaan saanut!”"/>
